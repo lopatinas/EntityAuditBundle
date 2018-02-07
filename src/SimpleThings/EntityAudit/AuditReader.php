30531feb4b7fe1805dbc97bfab82fd2adb6af572
Luciano Coggiola <lucianoc@cespi.unlp.edu.ar>
2018-02-07 15:24:12 -0300

    FIX #288: https://github.com/simplethings/EntityAuditBundle/issues/288

--- src/SimpleThings/EntityAudit/AuditReader.php
+++ src/SimpleThings/EntityAudit/AuditReader.php
@@ -232,10 +232,8 @@ class AuditReader
         $whereSQL  = "e." . $this->config->getRevisionFieldName() ." <= ?";
 
         foreach ($class->identifier AS $idField) {
-            if (is_array($id) && count($id) > 0) {
-                $idKeys = array_keys($id);
-                $columnName = $idKeys[0];
-            } else if (isset($class->fieldMappings[$idField])) {
+
+            if (isset($class->fieldMappings[$idField])) {
                 $columnName = $class->fieldMappings[$idField]['columnName'];
             } elseif (isset($class->associationMappings[$idField])) {
                 $columnName = $class->associationMappings[$idField]['joinColumns'][0];
